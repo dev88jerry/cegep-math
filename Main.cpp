35b961e7a245a5a64a76ebc05c48083dd444d0c4
dev88jerry <70291890+dev88jerry@users.noreply.github.com>
2020-08-31 17:59:46 -0400

    First step

---
+++ Main.cpp
@@ -0,0 +1,23 @@
+/*
+Jerry Lau
+Math-pro1
+main.cpp
+
+Base converision program
+
+*/
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+
+
+	system("PAUSE");
+
+	return 0;
+}
